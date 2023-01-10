85a6be2a625b92b24c0e8050d8b69ffa26f48f24
nils bundi <nbundi@proton.me>
2023-01-10 14:13:50 +0100

    Deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+forge create --rpc-url ${GOERLI_RPC_URL} \
+    --constructor-args ${TOKEN_IN} ${TOKEN_OUT} 750 31536000 1706831999 ${DEPLOYER_ADDRESS} \
+    --private-key ${DEPLOYER_KEY} src/TokenConversion.sol:TokenConversion \
+    --etherscan-api-key ${ETHERSCAN_API_KEY} \
+    --verify
